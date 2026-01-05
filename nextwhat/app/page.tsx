baad0764048d36617003ddca1a95f48e47d64474
Harish B <harishbalaji147@gmail.com>
2026-01-05 19:17:20 +0530

    hi (#3)
    * I changed cart -> page.tx
    
    * i changed app -> page.tsx

--- nextwhat/app/page.tsx
+++ nextwhat/app/page.tsx
@@ -3,6 +3,8 @@ export default async function RootPage() {
   await new Promise(resolve => setTimeout(resolve, 3000))
   return (
     <div>
+      <h1>Welcome to NextWhat Page</h1>
+      <p>This is the main page of the Nexat application.</p>
       <List 
       items={[1,2,300]}
       render={(u)=><div>{u*2}</div>}
