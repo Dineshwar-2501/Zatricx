3812aaaaffee82fd4ae06dfd81653c30ae5c1f18
harish14Git <harishbalaji147@gmail.com>
2026-01-05 18:27:59 +0530

    first commit

--- nextwhat/app/cart/page.tsx
+++ nextwhat/app/cart/page.tsx
@@ -4,6 +4,7 @@ export default async function CartPage() {
     const cart=await getProducts()
     return (
         <div>
+            <h1>Cart Details</h1>
             {cart.map((prod)=>(
                 <p key={prod.id}>{prod.name}</p>
             ))}
